e01154a0ac026d90efeb936f449100d5226386d6
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-13 20:18:39 -0300

    logramos recibir el objeto de una peticion de postman

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,14 +1,20 @@
-export const test = (req, res)=>{
-    res.status(200)
-    res.send('Primera prueba desde el backend')
-}
+export const test = (req, res) => {
+  res.status(200);
+  res.send("Primera prueba desde el backend");
+};
 
-export const leerProductos = (req, res) => {
+export const leerProductos = (req, res) => {};
 
-}
-
-export const crearProducto  = (req, res) => {
-
-}
+export const crearProducto = async (req, res) => {
+  try {
+    //recibir el objeto que tengo que agregar
+    console.log(req.body)
+    //validar los datos del objeto
+    //guardar el objeto en la BD
+    //contestar al frontend si funciono o no
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 //agregar funciones para CRUD
