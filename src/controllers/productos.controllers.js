80ecdb504825b7afeb7610d229867eeff13cb1cb
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-18 20:06:50 -0300

    creamos producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto.js";
+
 export const test = (req, res) => {
   res.status(200);
   res.send("Primera prueba desde el backend");
@@ -8,12 +10,16 @@ export const leerProductos = (req, res) => {};
 export const crearProducto = async (req, res) => {
   try {
     //recibir el objeto que tengo que agregar
-    console.log(req.body)
+    // console.log(req.body);
     //validar los datos del objeto
     //guardar el objeto en la BD
+    const nuevoProducto = new Producto(req.body);
+    await nuevoProducto.save();
     //contestar al frontend si funciono o no
+    res.status(201).json({ mensaje: "El producto fue creado exitosamente" });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ mensaje: "Error al crear el producto" });
   }
 };
 
---
+++ src/models/producto.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 100,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 50,
+    max: 1000000,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (valor) => {
+        return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?(\.(jpg|jpeg|png|webp))$/.test(
+          valor
+        );
+      },
+    },
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ["Infusiones", "Batidos", "Dulce", "Salado"],
+  },
+  descripcion_breve: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 250,
+  },
+  descripcion_amplia: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 500,
+  },
+});
+
+const Producto = mongoose.model("producto", productoSchema);
+
+export default Producto;
