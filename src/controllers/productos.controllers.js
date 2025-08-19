9975f708988b74c7f073a775c0eb052a8314fd5d
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-18 21:34:15 -0300

    logramos editar un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -67,3 +67,18 @@ export const borrarProducto = async (req,res) => {
     res.status(500).json({ mensaje: "Error al borrar el producto" });
   }
 }
+
+export const editarProducto = async (req,res) => {
+  try {
+    //buscar prodcuto por id
+    const productoEditado = await Producto.findByIdAndUpdate(req.params.id, req.body);
+    if (!productoEditado) {
+      return res.status(404).json({ mensaje: "No se encontro el producto y no pudimos editarlo" });
+    }
+    //responder al front
+    res.status(200).json({mensaje: 'Producto editado exitosamente'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al editar el producto" });
+  }
+}
